f7bc6b8280d3c670c50d4098f7d0017dab1beef5
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2023-09-03 14:52:25 +0530

    Setup src , assets and App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// App.js
+import React from 'react';
+import {createAppContainer} from 'react-navigation';
+import AppNavigator from './screens/AppNavigator';
+
+// const AppContainer = createAppContainer(AppNavigator);
+
+export default function App() {
+  return <AppNavigator />;
+}
