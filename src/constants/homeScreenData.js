df7e154d460b27110ff782c6d71339aafcb57fd1
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2023-09-04 10:12:11 +0530

    Setup constants in src

---
+++ src/constants/homeScreenData.js
@@ -0,0 +1,82 @@
+export default [
+  {
+    id: 1,
+    title: 'SPOOKY AND MYSTERY STORIES',
+    image: require('../assets/images/halloween1.jpg'),
+  },
+  {
+    id: 2,
+    title: 'SPOOKY AND MYSTERY STORIES',
+    image: require('../assets/images/halloween2.jpg'),
+  },
+  {
+    id: 3,
+    title: 'SPOOKY AND MYSTERY STORIES',
+    image: require('../assets/images/halloween3.jpg'),
+  },
+  {
+    id: 4,
+    title: 'SPOOKY AND MYSTERY STORIES',
+    image: require('../assets/images/halloween4.jpg'),
+  },
+];
+
+export const cardData = [
+  {
+    id: 1,
+    title: 'The Legend of Sleepy Hollow',
+    coverImage: require('../assets/images/card1.jpg'),
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
+    pages: 32,
+    readPercentage: 20,
+    author: 'WASHINGTON IRVING',
+    price: 5.99
+  },
+  {
+    id: 2,
+    title: 'The Haunting of Hollowbrook',
+    coverImage: require('../assets/images/card5.jpg'),
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
+    pages: 64,
+    readPercentage: 95,
+    author: 'OLIVER DARKTHORN',
+    price: 3.99
+  },
+  {
+    id: 3,
+    title: 'THE SCARY HALLOWEEN',
+    coverImage: require('../assets/images/card3.jpg'),
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
+
+    pages: 65,
+    readPercentage: 76,
+    author: 'JOHN DOE',
+    price: 9.99
+  },
+  {
+    id: 4,
+    title: 'The Curse of Midnight Hollow',
+    coverImage: require('../assets/images/card4.jpg'),
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
+    pages: 34,
+    readPercentage: 88,
+    author: 'ELEANOR BLACKWOOD',
+    price: 8.99
+  },
+  {
+    id: 5,
+    title: 'The Mummy',
+    coverImage: require('../assets/images/card2.jpg'),
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
+
+    pages: 45,
+    readPercentage: 58,
+    author: 'ALLAN COLINS',
+    price: 5.99
+  },
+];
---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+export const Halloween1 = require('../assets/images/Halloween1');
+export const Halloween2 = require('../assets/images/Halloween2');
+export const Halloween3 = require('../assets/images/Halloween3');
+export const Halloween4 = require('../assets/images/Halloween4');
+
+export default {
+  Halloween1,
+  Halloween2,
+  Halloween3,
+  Halloween4,
+};
